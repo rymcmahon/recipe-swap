984fc85ed89cca1918546bddda4312b687fa8b88
rymcmahon <toedragdangle9@gmail.com>
2015-07-28 19:55:15 -0500

    changed paperclip large image size

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ActiveRecord::Base
 	has_many :comments
 	belongs_to :meal
 	belongs_to :user
-	has_attached_file :image, :styles => { :small => "100x100>", :med => "200x200>", :large => "1632x1224>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image, :styles => { :small => "100x100>", :med => "200x200>", :large => "514x386>" }, :default_url => "/images/:style/missing.png"
 	before_save :destroy_image?
 	validates_attachment :image, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }, :size => { :in => 0..3.megabytes }
 	validates :name, presence: true
