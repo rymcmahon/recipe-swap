764ad5b6d6a0014776a8fe0dd358b5f2b5a12801
rymcmahon <toedragdangle9@gmail.com>
2015-08-20 13:30:36 -0500

    added change password capability devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,10 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -39,26 +42,20 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  #Devise
-  config.action_mailer.default_url_options = { :host => 'localhost:: 3000' }
-
   #Paperclip
   Paperclip.options[:command_path] = "/usr/local/bin/"
 
+  #ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
-
+    address: "smtp.gmail.com",
     port: 587,
-
-  domain: ENV["GMAIL_DOMAIN"],
-
-  authentication: "plain",
-
-  enable_starttls_auto: true,
-
-  username: ENV["GMAIL_USERNAME"],
-
-  password: ENV["GMAIL_PASSWORD"]
-
-}
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150802173709) do
+ActiveRecord::Schema.define(version: 20150820180414) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body",       limit: 65535
@@ -66,8 +66,12 @@ ActiveRecord::Schema.define(version: 20150802173709) do
     t.boolean  "admin",                  limit: 1,   default: false
     t.string   "first_name",             limit: 255
     t.string   "last_name",              limit: 255
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
