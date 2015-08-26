836dccbfb83fe33611869dfc5116d769229d06e0
rymcmahon <toedragdangle9@gmail.com>
2015-08-26 08:42:27 -0500

    changed to action mailer host to totaste

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'http://totaste.org.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'www.totaste.org' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
